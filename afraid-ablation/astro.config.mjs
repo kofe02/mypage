03c134088886acb4290f031d02e5666c4860b52f
kofe <t2312121@gl.cc.uec.ac.jp>
2024-12-17 19:29:11 +0900

    ベースパスを'/mypage'に変更

--- afraid-ablation/astro.config.mjs
+++ afraid-ablation/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kofe02.github.io',
-    base: '/mypage/afraid-ablation',
+    base: '/mypage',
 })
